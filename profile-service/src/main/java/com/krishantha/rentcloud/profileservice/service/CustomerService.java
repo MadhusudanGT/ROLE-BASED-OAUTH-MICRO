cf8ca117b546ebfec298b0443063bfec61dce5a8
MADHU <65445352+MadhusudanGT@users.noreply.github.com>
2021-02-23 16:03:26 +0530

    Add files via upload

---
+++ profile-service/src/main/java/com/krishantha/rentcloud/profileservice/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.krishantha.rentcloud.profileservice.service;
+
+import com.krishantha.rentcloud.profileservice.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+
+    Customer save(Customer customer);
+
+    Customer fetchById(int profileId);
+
+    List<Customer> fetchAllProfiles();
+}
